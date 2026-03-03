98e84dd17456724a6ffccf38b86d565b7615c36e
Link4Electronics <link4electronics@gmail.com>
2026-03-03 18:28:30 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,7 +7,6 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
